2d6cbe2943a6535f8f7a840ab210c9f096babde2
grabtom <35785223+grabtom@users.noreply.github.com>
2018-12-13 14:49:09 +0100

    Update 2018

--- deerHunt.js
+++ deerHunt.js
@@ -1,40 +1,58 @@
-Reindeer = function(refreshTime, jumpPages) {
-    refreshTime = refreshTime || 5000;
-    var searchInterval = null;
-    var jumpPageTimeout = null;
+// ==UserScript==
+// @name         Pepper reniferek
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.pepper.pl/*
+// @grant        none
+// ==/UserScript==
 
-    var lookForReindeer = function() {
-        console.log('lookedForDeer');
-        var found = false;
-        var reinders = $("[src*='mascotcard']");
-        if (reinders.length) {
-            found = true;
-            clearInterval(searchInterval);
-            clearTimeout(jumpPageTimeout);
-            console.log('should notify');
-            if (Notification.permission !== 'denied') {
-                Notification.requestPermission(function (permission) {
-                    if (permission === "granted") {
-                        var notification = new Notification("JEST RENIFER HHHURWA!!!");
-                    }
-                });
+(function() {
+    'use strict';
+
+    const Reindeer = (function(refreshTime, jumpPages) {
+        refreshTime = refreshTime || 5000;
+        let searchInterval = null;
+        let jumpPageTimeout = null;
+
+        const lookForReindeer = function() {
+            console.log('Szukam reniferka');
+            let found = false;
+            const reinder = document.querySelector('.mc-btn--primary');
+            if (reinder) {
+                found = true;
+                clearInterval(searchInterval);
+                clearTimeout(jumpPageTimeout);
+                console.log('Jest reniferek! Klikam!');
+                reinder.click();
+                console.log('Za 60 sekund przeładuję stronę');
+                setTimeout(() => {
+                    location.reload();
+                }, 60000);
             }
+        };
+
+        if (jumpPages) {
+            const cards = document.querySelectorAll(
+                'div.gridLayout-item.threadCardLayout--card'
+            );
+            const timeout = Math.floor(Math.random() * 120) + 15;
+            console.log(
+                `Za ${timeout} sekund kliknę w jakąś ofertę lub wrócę na stronę główną`
+            );
+            jumpPageTimeout = setTimeout(function() {
+                if (location.href !== 'https://www.pepper.pl/') {
+                    console.log('Wracam na stronę gówną');
+                    location.href = 'https://www.pepper.pl/';
+                }
+                const item = cards[
+                    Math.floor(Math.random() * (cards.length - 1))
+                ].querySelector('a');
+                item.click();
+            }, timeout * 1000);
         }
-    }
-    
 
-
-    if (jumpPages) {
-        var links = $('a').filter(function(link) {
-            return $(this).attr('href') && $(this).attr('href').indexOf('pepper.pl') > -1
-        });
-        var timeout =  (Math.floor(Math.random() * 120) + 15);
-        jumpPageTimeout = setTimeout(function() {
-            var item = links[Math.floor(Math.random()*links.length)];
-            item.click();
-        }, timeout * 1000 );
-    }
-
-    searchInterval = setInterval(lookForReindeer, 5000);
-
-}(2000, true);
+        searchInterval = setInterval(lookForReindeer, 5000);
+    })(2000, true);
+})();
